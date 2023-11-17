d10ae3fa66107fb5bf72f25234b8e1e489624243
DecryptLife <benson.thomas2602@gmail.com>
2023-11-17 01:02:02 -0600

    general suggestions/guidelines

--- server/index.js
+++ server/index.js
@@ -2,6 +2,12 @@ const express = require("express");
 const app = express();
 const http = require("http");
 
+// when a user logs in use password hashing: never store plain-text password. A library that could be used to do this - bcrypt
+
+// implement rate limiting on login attemps to prevent brute force attempts
+
+// after successful login - generate token and send it back to the user (JWT), store token on client side securely - HTTP only cookies to prevent XSS attacks
+
 app.listen(3000, () => {
   console.log("Server listening on 3000");
 });
---
+++ server/middleware/auth.js
@@ -0,0 +1,1 @@
+// check for the authentication token in the headers after the user logs in to ensure that only
